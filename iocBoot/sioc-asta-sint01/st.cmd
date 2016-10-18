34591cfb23abf860c4bb1d2065442011c8f762aa
Michael Dunning <mdunning@slac.stanford.edu>
2016-10-18 13:11:39 -0700

    First commit.

---
+++ iocBoot/sioc-asta-sint01/st.cmd
@@ -0,0 +1,28 @@
+#!../../bin/linux-x86_64/softInt
+
+< envPaths
+
+cd ${TOP}
+
+dbLoadDatabase "dbd/softInt.dbd"
+softInt_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet("N", "01")
+epicsEnvSet("P", "ASTA:SOFTINT$(N)")
+
+save_restoreSet_status_prefix("")
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+set_savefile_path("/nfs/slac/g/testfac/asta/$(IOC)", "autosave")
+set_pass0_restoreFile("softInt.sav")
+set_pass1_restoreFile("softInt.sav")
+
+dbLoadRecords("db/softInt-asta.db", "P=$(P)")
+
+#cd ${AUTOSAVE}
+#dbLoadRecords("db/save_restoreStatus.db", "P=$(P)")
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+create_monitor_set("softInt.req", 30, "P=$(P)")
